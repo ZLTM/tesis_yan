fab216bceb0ffb5ab1adbefa344bd29ba2114b7a
zltm <jdavidrevillad@gmail.com>
2020-07-06 13:13:07 -0400

    names fixed

--- app/src/main/java/com/example/senthil/kotlin_tablayout/Fragment/RegistroFragment.kt
+++ app/src/main/java/com/example/senthil/kotlin_tablayout/Fragment/RegistroFragment.kt
@@ -1,20 +1,25 @@
 package com.example.senthil.kotlin_tablayout.Fragment
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.example.senthil.kotlin_tablayout.R
+
 
 class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        val receivingFragment = AboutUsFragment()
+        val args = Bundle()
+        args.putString("Key", "Value")
+        receivingFragment.setArguments(args)
+
+        fragmentManager!!.beginTransaction().add(R.id.container, receivingFragment).commit()
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
 }
